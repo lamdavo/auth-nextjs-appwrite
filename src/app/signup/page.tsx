d60b25f6a3028b34fc67c81e0674608bb2e31b66
Lam <vo2022@protonmail.com>
2023-07-27 14:50:45 -0700

    initial

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+
+export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    userName: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Sign up</h1>
+      <hr />
+      <label htmlFor="username"> username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        id="username"
+        value={user.userName}
+        onChange={(e) => setUser({ ...user, userName: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email"> email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password"> password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onSignup}
+      >
+        Sign up here
+      </button>
+      <Link href="/login"> Visit login </Link>
+    </div>
+  );
+}
---
+++ src/dbconfig/dbconfig.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("Database connected successfully");
+    });
+
+    connection.on("error", (error) => {
+      console.log("Error connecting to database: ", error);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Error connecting to database: ", error);
+  }
+}
